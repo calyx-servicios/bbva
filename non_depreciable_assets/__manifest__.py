64b1ebfd1c618645438d85efc6591094439f4e2b
carlamiquetan <carlamiquetan@gmail.com>
2023-03-15 11:06:31 -0300

    [ODOO-6024][IMP][non_depreciable_assets] -Non-amortizable assets.

---
+++ non_depreciable_assets/__manifest__.py
@@ -0,0 +1,29 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Non Depreciable Assets",
+    "summary": """
+        This module makes the amortization tables not amortize
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["carlamiquetan"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": ["base",
+                "account_asset_management",
+                "account_asset_batch_compute",
+                "account_asset_management_menu",
+                ],
+
+    ### XML Data files
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'views/views.xml',
+    #     'views/templates.xml',
+    # ],
+}
